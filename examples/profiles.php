38c42ecb3ef81ec384b2ea11ac4b5fb2ae0a4c77
Patrick Lam <zidizei@gmail.com>
2014-03-22 23:37:53 +0100

    more examples

--- examples/profiles.php
+++ examples/profiles.php
@@ -14,5 +14,17 @@ require "../vendor/autoload.php";
 /* Parse the stuff */
 \Debug\debug("Stuff successfully parsed");
 
+
+// initiate debug profile named 'worker'
+\Debug\profile("worker");
+
+\Debug\debug("Working with the stuff now");
+/* Work with the parsed stuff */
+\Debug\debug("Stuff successfully worked with.. or something like that.");
+
+// close the current debug profile (in this case 'worker')
+\Debug\profile();
+
+
 // close the current debug profile (in this case 'parser')
 \Debug\profile();
