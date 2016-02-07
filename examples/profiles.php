a0b510524f4614af596ff1630889e7b5d789a623
Patrick Lam <zidizei@gmail.com>
2016-02-07 23:51:03 +0100

    add timeouts to the profiles example

--- examples/profiles.php
+++ examples/profiles.php
@@ -10,21 +10,29 @@ require "../vendor/autoload.php";
 // initiate debug profile named 'parser'
 \Debug\profile("parser");
 
+usleep(500 * 1000);
+
 \Debug\debug("Parsing some stuff now");
+
 /* Parse the stuff */
+usleep(1000 * 1000);
+
 \Debug\debug("Stuff successfully parsed");
 
+usleep(100 * 1000);
 
 // initiate debug profile named 'worker'
 \Debug\profile("worker");
 
 \Debug\debug("Working with the stuff now");
+
 /* Work with the parsed stuff */
+usleep(600 * 1000);
+
 \Debug\debug("Stuff successfully worked with.. or something like that.");
 
 // close the current debug profile (in this case 'worker')
 \Debug\profile();
-
 
 // close the current debug profile (in this case 'parser')
 \Debug\profile();
